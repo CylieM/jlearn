2636f016da898c24329b18b99a4935417b5ac30c
JohnBryanRamon <ramonjohn2010@gmail.com>
2024-05-13 17:10:03 +0800

    Added ForgotPasswordB

--- app/src/main/java/com/example/jlearnn/ForgotPasswordBActivity.java
+++ app/src/main/java/com/example/jlearnn/ForgotPasswordBActivity.java
@@ -8,31 +8,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordBActivity extends AppCompatActivity {
 
-    public EditText etEmail;
-     Button btnResetPassword;
+    private EditText editPassword;
+    private Button btnResetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        etEmail = findViewById(R.id.etEmail);
+        editPassword = findViewById(R.id.editPassword);
         btnResetPassword = findViewById(R.id.btn_reset_password);
 
         btnResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = etEmail.getText().toString();
+                String password = editPassword.getText().toString();
 
-                if (email.isEmpty()) {
-                    Toast.makeText(ForgotPasswordActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                if (password.isEmpty()) {
+                    Toast.makeText(ForgotPasswordBActivity.this, "Please enter your new password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 // Call API or perform password reset logic here
-                Toast.makeText(ForgotPasswordActivity.this, "Password reset link sent to your email", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ForgotPasswordBActivity.this, "Password re - enter your new password.", Toast.LENGTH_SHORT).show();
             }
         });
     }
